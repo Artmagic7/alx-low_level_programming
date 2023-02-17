8d0335c0f8d5e7d56172e0f39faee2c88d8fa4ae
Arthur Owens <arth.owens@gmail.com>
2023-02-17 13:16:30 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower and upper case
+ * Return: Always 0
+ */
+int (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
