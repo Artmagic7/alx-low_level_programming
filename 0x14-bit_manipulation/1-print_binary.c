e539364c8e3b3777edb523d3fd2c4c0220d1b8f8
arthur owens <arth.owens@gmail.com>
2023-04-12 11:34:38 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,14 +6,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << 63; /* mask to isolate highest bit */
+	unsigned long int mask = 1;
+	int len = 0;
 
-	while (mask > 0) /* loop through all bits */
+	/* find the length of the binary representation */
+	while ((n >> len) != 0)
+		len++;
+
+	/* print the binary representation */
+	if (len == 0)
+		_putchar('0');
+	else
 	{
-		if ((n & mask) == 0) /* bit is 0 */
-			_putchar('0');
-		else /* bit is 1 */
-			_putchar('1');
-		mask >>= 1; /* shift mask to next bit */
+		while (len--)
+		{
+			if ((n & (mask << len)) != 0)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
 	}
 }
