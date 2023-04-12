972a6aaba14ab47fa4dc111ed1139a15858bbac7
arthur owens <arth.owens@gmail.com>
2023-04-12 11:49:34 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,14 +5,14 @@
  * @n: number to search
  * @index: index of the bit
  *
- * Return: value of the bit at index or -1 if an error occurred
+ * Return: value of the bit or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index > 63)
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
+	unsigned long int mask = 1ul << index;
 
 	return ((n & mask) != 0);
 }
