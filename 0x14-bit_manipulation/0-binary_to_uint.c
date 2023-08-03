e3e9c3daacefbb416da5c8007174dbd7e142b8e5
Artmagic7 <arth.owens@gmail.com>
2023-08-03 10:43:42 -0700

    Bin to Interger

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* binary_to_uint - a func that converts a binary number to an unsigned int
-* @b: The Binary num String
-* Return: The Binary num interger
-*/
+ * binary_to_uint - A Binary num is converted to unsigned int
+ * @b: The Binary num within the string
+ *
+ * Return: The converted num, to 0 if b is NULL or to the coverted num
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
 	int p;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (p = 0; b[p] != '\0'; p++)
+
+	for (p = 0; b[p]; p++)
 	{
 		if (b[p] != '0' && b[p] != '1')
 			return (0);
-		num <<= 1;
-		num += b[p] - '0';
+		dec_val = 2 * dec_val + (b[p] - '0');
 	}
-	return (num);
+
+	return (dec_val);
 }
