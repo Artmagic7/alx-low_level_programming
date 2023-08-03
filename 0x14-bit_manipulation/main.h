29fa8c6c07010cb25911a56c6c59315c8dbeb895
Artmagic7 <arth.owens@gmail.com>
2023-08-03 11:04:12 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@ int get_endianness(void);
 void print_binary(unsigned long int n);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _atoi(const char *s);
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uintt(const char *b);
 int _putchar(char c);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_bit(unsigned long int n, unsigned int index);
